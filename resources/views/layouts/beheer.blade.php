6ad4f34165e3ff017e7ccaf04b8d6bb431c205a7
Maarten de Jager <maartendejager@gmail.com>
2016-06-26 20:38:36 +0100

    first steps toward new layout

--- resources/views/layouts/beheer.blade.php
+++ resources/views/layouts/beheer.blade.php
@@ -18,32 +18,36 @@
 	    	<div class="off-canvas-content" data-off-canvas-content>
 
 			    <main>
-					<img src="{{asset('img/paperclip.png')}}" id="paperclip">
-					<div class="top_bar">
+
+					<div class="top_bar clearfix">
 						<div class="top_bar--box float-left">
 							<div class="navbarleft__hamburger" data-toggle="offCanvas"></div>
 						</div>
 
 		  				@include('layouts.partials.edituser')
 					</div>
+	
+					<div class="content-group">
 
-					<div class="sidetext">
-						<div class="sidetext__inner">
-							<span class="sidetext__title">Implementatiescan</span>
-							@if(isset($scan))
-								<span class="sidetext__sub">Regio {{ $scan->regio }}</span>
-							@endif
+						<div class="sidetext">
+							<div class="sidetext__inner">
+								<span class="sidetext__title">Implementatiescan</span>
+								@if(isset($scan))
+									<span class="sidetext__sub">Regio {{ $scan->regio }}</span>
+								@endif
+							</div>
 						</div>
-					</div>
 
 
-					<div id="content">
+						<div id="content">
 
-						@if(Session::has('global'))
-							{{ Session::get('global') }}
-						@endif
+							@if(Session::has('global'))
+								{{ Session::get('global') }}
+							@endif
 
-						@yield('content')
+							@yield('content')
+						
+						</div>
 					
 					</div>
 				</main>
