daa47491a32ac77b7e3604dfb21b165a55527875
Maarten de Jager <maartendejager@gmail.com>
2016-08-04 11:23:04 +0100

    changed seed

--- database/seeds/RoleUsersTableSeeder.php
+++ database/seeds/RoleUsersTableSeeder.php
@@ -15,7 +15,7 @@ class RoleUsersTableSeeder extends Seeder
 
         $role_user = [
             [
-                'role_id' => '1',
+                'role_id' => '2',
                 'user_id' => '1'
             ],
             [
@@ -26,6 +26,10 @@ class RoleUsersTableSeeder extends Seeder
                 'role_id' => '3',
                 'user_id' => '3'
             ],
+            [
+                'role_id' => '2',
+                'user_id' => '1'
+            ]
         ];
 
         DB::table('role_user')->insert($role_user);
