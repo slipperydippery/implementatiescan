17ad5c79a1367f9ba5851a206c4749219191206e
Maarten de Jager <maartendejager@gmail.com>
2016-09-19 22:35:37 +0100

    added email sender and name

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@implementatiescan.nl', 'name' => 'Team implementatiescan'],
 
     /*
     |--------------------------------------------------------------------------
