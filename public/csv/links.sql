40c3e829f2ac65ecb9483f41d6c43036dd2d34bd
Maarten de Jager <maartendejager@gmail.com>
2016-11-22 19:23:54 +0000

    transfer sql file

--- public/csv/links.sql
+++ public/csv/links.sql
@@ -20,7 +20,18 @@ INSERT INTO `links` (`id`, `title`, `description`, `adress`, `created_at`, `upda
 	(4, 'www.vobc.nu', ' ', 'http://www.vobc.nu', NULL, NULL),
 	(5, 'www.lcr.nl', ' ', 'http://www.lcr.nl', NULL, NULL),
 	(6, 'www.bvekennis.nl', ' ', 'http://www.bvekennis.nl', NULL, NULL),
-	(7, 'www.mboraad.nl', ' ', 'http://www.mboraad.nl', NULL, NULL);
+	(7, 'www.mboraad.nl', ' ', 'http://www.mboraad.nl', NULL, NULL),
+	(8, 'Rijksoverheid', 'Informatie over beleid en regelgeving in Nederland omtrent arbeid en inkomen', 'https://www.rijksoverheid.nl/onderwerpen/participatiewet', NULL, NULL),
+	(9, 'UWV', 'Informatie over beoordeling arbeidsvermogen en uitkeringen', 'http://www.uwv.nl/', NULL, NULL),
+	(10, 'Nederlands Jeugd Instituut', 'Kennis over jeugd-, onderwijs- en opvoedingsvraagstukken', 'http://www.nji.nl/', NULL, NULL),
+	(11, 'Kennisnet jeugd', 'Platform voor het delen van kennis en informatie over jeugd-, onderwijs- en opvoedingsvraagstukken', 'https://kennisnetjeugd.nl/', NULL, NULL),
+	(12, 'Landelijke expertise centrum speciaal onderwijs', 'Informatie over het speciaal onderwijs. Informatie over passend onderwijs, onderwijs en zorg, onderwijskwaliteit en arbeidstoeleiding', 'https://www.lecso.nl/', NULL, NULL),
+	(13, 'Kenniscentrum LVB', 'Informatie over de persoonlijke ontwikkeling, opvoedingssituatie en maatschappelijke participatie van mensen met een licht verstandelijke beperking.', 'http://www.kenniscentrumlvb.nl/', NULL, NULL),
+	(14, 'Het Kenniscentrum Arbeidstoeleiding Nederland', 'Informatie omtrent arbeidstoeleiding van jongeren uit het speciaal onderwijs', 'http://www.hetkan.info/', NULL, NULL),
+	(15, 'Divosa', 'Informatie over vraagstukken in het bredere sociaal domein', 'http://www.divosa.nl/', NULL, NULL),
+	(16, 'Ingrado', 'Informatie over leerplicht en RMC (voortijdig schoolverlaten).', 'http://www.ingrado.nl/', NULL, NULL),
+	(17, 'Landelijke clienten raad', 'Informatie over wetten en regels over werk en inkomen en clientenparticipatie', 'http://www.landelijkeclientenraad.nl', NULL, NULL),
+	(18, 'MEE', 'Informatie over ondersteuning van mensen met een beperking en hun netwerk op alle leefgebieden en in alle levensfasen.', 'http://www.mee.nl/', NULL, NULL);
 /*!40000 ALTER TABLE `links` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
